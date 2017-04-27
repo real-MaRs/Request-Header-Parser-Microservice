afbbc8c5abfd73e2f518a2869b50a34e670107f8
Justin Chong <just.inchong4life@gmail.com>
2017-04-26 21:24:50 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const accepts = require('accepts');
-const os = require('os');
+const useragent = require('express-useragent');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+app.use(useragent.express());
+
+app.get('/', (req, res) => {
+  res.send('type /whoami after url to get info');
+});
 
 app.get('/whoami', (req, res) => {
   res.json({
     ipaddress: req.ip,
     language: accepts(req).languages()[0],
-    operatingsystem: os.type()
+    operatingsystem: req.useragent.os
   });
 });
 
-app.listen(3000);
-console.log('listening to port 3000');
+app.listen(process.env.PORT || 3000, () => console.log('app running'));
